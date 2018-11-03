468a094820397e16df92e296bf4f68fd49ca122c
Mark Kuczmarski <markkuczmarski@gmail.com>
2018-11-04 08:52:35 +0900

    Shuffle array and more questions in checklist

--- markact.js
+++ markact.js
@@ -9,6 +9,26 @@ function h(nodeName, attributes, ...args) {
     };
 }
 
+function shuffle(array) {
+    let counter = array.length;
+
+    // While there are elements in the array
+    while (counter > 0) {
+        // Pick a random index
+        let index = Math.floor(Math.random() * counter);
+
+        // Decrease counter by 1
+        counter--;
+
+        // And swap the last element with it
+        let temp = array[counter];
+        array[counter] = array[index];
+        array[index] = temp;
+    }
+
+    return array;
+}
+
 function render(vnode) {
     if (vnode.split) return document.createTextNode(vnode);
     let n = document.createElement(vnode.nodeName);
